3793485c2e4c872e6f601f3184098db7b1823f21
ermiry <erick.salas@ermiry.com>
2021-01-04 17:13:19 -0600

    Refactored version methods

--- src/cmongo/version.c
+++ src/cmongo/version.c
@@ -5,22 +5,22 @@
 // print full erver version information
 void cmongo_version_print_full (void) {
 
-	printf ("\nCmongo Version: %s\n", CMONGO_VERSION_NAME);
-	printf ("Release Date & time: %s - %s\n", CMONGO_VERSION_DATE, CMONGO_VERSION_TIME);
-	printf ("Author: %s\n\n", CMONGO_VERSION_AUTHOR);
+	(void) printf ("Cmongo Version: %s\n", CMONGO_VERSION_NAME);
+	(void) printf ("Release Date & time: %s - %s\n", CMONGO_VERSION_DATE, CMONGO_VERSION_TIME);
+	(void) printf ("Author: %s\n", CMONGO_VERSION_AUTHOR);
 
 }
 
 // print the version id
 void cmongo_version_print_version_id (void) {
 
-	printf ("\nCmongo Version ID: %s\n", CMONGO_VERSION);
+	(void) printf ("Cmongo Version ID: %s\n", CMONGO_VERSION);
 
 }
 
 // print the version name
 void cmongo_version_print_version_name (void) {
 
-	printf ("\nCmongo Version: %s\n", CMONGO_VERSION_NAME);
+	(void) printf ("Cmongo Version: %s\n", CMONGO_VERSION_NAME);
 
 }
