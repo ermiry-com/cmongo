91e99bd237a5d6ad6dafaea9b2917319963b22ff
ermiry <erick.salas@ermiry.com>
2021-01-04 16:21:58 -0600

    Added cmongo config header with custom definitions

---
+++ include/cmongo/config.h
@@ -0,0 +1,13 @@
+#ifndef _CMONGO_CONFIG_H_
+#define _CMONGO_CONFIG_H_
+
+// to be used in the application
+#define CMONGO_EXPORT 			extern
+
+// to be used by internal cerver methods
+#define CMONGO_PRIVATE			extern
+
+// used by private methods but can be used by the application
+#define CMONGO_PUBLIC 			extern
+
+#endif
