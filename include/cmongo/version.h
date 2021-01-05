17b23fdb0cd8188f87b3834ad38f8583d25565b2
ermiry <erick.salas@ermiry.com>
2021-01-04 23:28:32 -0600

    Updated version date & time

--- include/cmongo/version.h
+++ include/cmongo/version.h
@@ -3,8 +3,8 @@
 
 #define CMONGO_VERSION						"1.0"
 #define CMONGO_VERSION_NAME					"Release 1.0"
-#define CMONGO_VERSION_DATE					"04/12/2020"
-#define CMONGO_VERSION_TIME					"18:21 CST"
+#define CMONGO_VERSION_DATE					"04/01/2021"
+#define CMONGO_VERSION_TIME					"23:28 CST"
 #define CMONGO_VERSION_AUTHOR				"Erick Salas"
 
 #ifdef __cplusplus
