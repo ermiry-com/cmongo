3536c6b11ab22571612d71f96c9e1b68c03f74f1
ermiry <erick.salas@ermiry.com>
2021-03-02 12:23:05 -0600

    Added base dedicated model structure & methods

---
+++ include/cmongo/model.h
@@ -0,0 +1,39 @@
+#ifndef _CMONGO_MODELS_H_
+#define _CMONGO_MODELS_H_
+
+#include <stddef.h>
+
+#include <bson/bson.h>
+
+#include "cmongo/config.h"
+
+#define CMONGO_COLLNAME_SIZE		128
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef void (*mongo_parser)(void *model, const bson_t *doc);
+
+typedef struct CMongoModel {
+
+	size_t collname_len;
+	char collname[CMONGO_COLLNAME_SIZE];
+
+	mongo_parser model_parser;
+
+} CMongoModel;
+
+CMONGO_PUBLIC CMongoModel *cmongo_model_new (void);
+
+CMONGO_PUBLIC void cmongo_model_delete (void *model_ptr);
+
+CMONGO_EXPORT CMongoModel *cmongo_model_create (
+	const char *collname
+);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
