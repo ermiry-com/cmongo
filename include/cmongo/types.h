688814c433156fc48abc575beeef793fca939638
ermiry <erick.salas@ermiry.com>
2021-04-02 21:02:51 -0600

    Added methods to print oids & documents

--- include/cmongo/types.h
+++ include/cmongo/types.h
@@ -5,6 +5,7 @@
 
 #include "cmongo/config.h"
 
+#define BSON_OID_BUFFER_SIZE		32
 #define BSON_ARRAY_BUFFER_SIZE		16
 
 #ifdef __cplusplus
@@ -18,6 +19,10 @@ CMONGO_PUBLIC void bson_oid_delete (void *bson_oid_t_ptr);
 CMONGO_PUBLIC bson_oid_t *bson_oid_create (
 	const bson_oid_t *original_oid
 );
+
+CMONGO_PUBLIC void bson_oid_print (const bson_oid_t *oid);
+
+CMONGO_PUBLIC void bson_print_as_json (const bson_t *document);
 
 #ifdef __cplusplus
 }
